75e137596de55aac095c085565c66733cd34d4f3
Ian Lee <IanLee1521@gmail.com>
2021-06-18 21:10:32 +0000

    Added new stuff module

---
+++ awesome/stuff.py
@@ -0,0 +1,11 @@
+import requests
+
+def get_page_size(url):
+    print(f'querying url: {url}')
+    response = requests.get(url)
+
+    print(f'Got status code: {response.status_code}')
+    print(f"Page html is {len(response.text)} characters long")
+
+def demo():
+        get_page_size("https://wildwesthackinfest.com")
